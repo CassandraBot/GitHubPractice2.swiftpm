5849a957e844f4c4467be4406e69d2cb75ba83c2
Cassandra Botnari <cbotnari8050@stu.d214.org>
2025-04-10 11:02:25 -0500

    added variable answer

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
+    @State var answer = 0
     var body: some View {
         TextField("Enter a #", value: $number2, format: .number)
         TextField("Enter a #", value: $number1, format: .number)
